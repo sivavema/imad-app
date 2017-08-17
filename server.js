44938bb4febd5a89255e689248cbf1541a3f9e6b
sivavema <sivakrishnareddyp1@gmail.com>
2017-08-17 11:51:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone',function(req, res){
-    res.send('article one was requested and was send here');
+    res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get('/articletwo',function(req, res){
     res.send('article two was requested and was send here');
